728bd1ef1b5f4f009cc0d54767ff4709cdb4742f
Sebastiaan Saarloos <sebastiaan.saarloos@gmail.com>
2019-05-15 15:33:41 +0200

    Defined headers for #1

--- code/headers/frame_s.hpp
+++ code/headers/frame_s.hpp
@@ -4,8 +4,17 @@
 #include <cstdint>
 
 namespace r2d2::communication {
+    /**
+     * @brief Generic frame type that contains an byte array/pointer with length.
+     */
     struct frame_s {
+        /**
+         * @brief Size of the data in bytes (1 byte, 8 bits equals 1)
+         */
         size_t size;
+        /**
+         * @brief Frame data
+         */
         uint8_t data[];
     };
 }
