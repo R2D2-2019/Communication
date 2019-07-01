1fd1c3d7ec0efeac4a02a39fd338676252c1052d
Patrick Dekker <Patrick_Dekker@hotmail.com>
2019-07-01 13:16:26 +0200

    re added the slave select

--- code/headers/esp_32.hpp
+++ code/headers/esp_32.hpp
@@ -14,6 +14,7 @@ namespace r2d2::communication {
     class esp_32_c : public external_connection_c {
     private:
         hwlib::spi_bus &spi_connection;
+        hwlib::pin_out &slave_select;
         hwlib::pin_in &hand_shake;
         r2d2::queue_c<r2d2::frame_external_s, 32> recv_queue;
 
@@ -21,7 +22,10 @@ namespace r2d2::communication {
         /**
          * @brief the constructor for the esp32 interface
          *
+         * @details slave select should be a pin_out dummy when using the hwspi implementation
+         *
          * @param spi_connection The spi bus connected to the esp
+         * @param ss slave select of chip select connected to the esp
          * @param hs The hand shake pin connected to the esp
          */
         esp_32_c(hwlib::spi_bus &spi_connection, hwlib::pin_in &hs);
--- code/src/esp_32.cpp
+++ code/src/esp_32.cpp
@@ -2,8 +2,10 @@
 
 namespace r2d2::communication {
 
-    esp_32_c::esp_32_c(hwlib::spi_bus &spi_connection, hwlib::pin_in &hs)
-        : spi_connection(spi_connection), hand_shake(hs) {
+    esp_32_c::esp_32_c(hwlib::spi_bus &spi_connection, hwlib::pin_out &ss, hwlib::pin_in &hs)
+        : spi_connection(spi_connection),
+          slave_select(ss)
+          hand_shake(hs){
     }
 
     void esp_32_c::send(const r2d2::frame_external_s &frame) {
@@ -12,7 +14,7 @@ namespace r2d2::communication {
         uint8_t recv[256] = {0};
         // wait till esp is ready
         while (!hand_shake.read()){}
-        spi_connection.transaction(hwlib::pin_out_dummy)
+        spi_connection.transaction(ss)
             .write_and_read(frame.length,
                             reinterpret_cast<const uint8_t *>(&frame), recv);
         // if the length of the external frame received is 0 assume nothing was send
@@ -30,7 +32,7 @@ namespace r2d2::communication {
             uint8_t recv[256] = {0};
             // wait till esp is ready
             while (!hand_shake.read()){}
-            spi_connection.transaction(hwlib::pin_out_dummy).write_and_read(256, nullptr, recv);
+            spi_connection.transaction(ss).write_and_read(256, nullptr, recv);
             // if the length of the external frame received is 0 assume nothing was send
             // and don't put the frame in the receive que
             if (recv[0] != 0) {
