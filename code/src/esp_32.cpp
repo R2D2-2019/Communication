71c99071f8f2df38c18fd2ce22b7c5ba0f6e61d5
Patrick Dekker <patrick_dekker@hotmail.com>
2019-07-03 10:24:04 +0200

    fixed some things for demo

--- code/src/esp_32.cpp
+++ code/src/esp_32.cpp
@@ -12,11 +12,20 @@ namespace r2d2::communication {
         // set the first bite of the buffer to 0
         // only the first because the first byte of an external frame is its length
         uint8_t recv[256] = {0};
-        // wait till esp is ready
-        while (!hand_shake.read()){}
+        uint8_t send[256] = {frame.length, frame.id.octets[0], frame.id.octets[1], frame.type};
+                memcpy(send+4, frame.data, frame.length );
+        //for (uint8_t i = 0; i < frame.length + 1 ; i++) {
+            //hwlib::cout << send[i];
+        //}
+        //hwlib::cout << '\n';
+
+// wait till esp is ready
+        while (!hand_shake.read()){hwlib::cout << 1;}
+        //spi_connection.transaction(slave_select)
+        //    .write_and_read(frame.length ,
+        //                    reinterpret_cast<const uint8_t *>(&frame), recv);
         spi_connection.transaction(slave_select)
-            .write_and_read(frame.length,
-                            reinterpret_cast<const uint8_t *>(&frame), recv);
+            .write_and_read(5, send, recv);
         // if the length of the external frame received is 0 assume nothing was send
         // and don't put the frame in the receive que
         if (recv[0] != 0) {
